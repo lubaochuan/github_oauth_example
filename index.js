46ffdbcce365c9dda177fbf65d0d55d48d9f7341
Baochuan Lu <blu@sbuniv.edu>
2022-12-13 21:49:00 +0000

    add oauth-callback route

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 
 const app = express();
-
-const clientId = 'OMITTED';
-const clientSecret = 'OMITTED';
+const axios = require('axios');
+let token = null;
 
 app.get('/', (req, res) => {
   res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}`);
+});
+
+app.get('/oauth-callback', (req, res) => {
+  const body = {
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    code: req.query.code
+  };
+  const opts = { headers: { accept: 'application/json' } };
+  axios.post(`https://github.com/login/oauth/access_token`, body, opts).
+    then(res => res.data['access_token']).
+    then(_token => {
+      token = _token;
+      console.log('My token:', token);
+      res.json({ ok: 1 });
+    }).
+    catch(err => res.status(500).json({ message: err.message }));
 });
 
 app.listen(3000);
